1ea80779f14bb3657a009d8afe77919af3b198bc
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-19 09:11:31 -0500

    Model complete

--- .history/models/book_20200518153023.js
+++ .history/models/book_20200518153023.js
@@ -3,10 +3,9 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  authors: { type: Array, default: [] },
-  description: String,
-  image: String,
-  link: String
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);
